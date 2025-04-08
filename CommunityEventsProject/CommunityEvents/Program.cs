d6019646778f2d223bfdf463b588012c8c3fcd4e
windowmipers <98720261+windowmipers@users.noreply.github.com>
2025-04-08 04:30:15 +0000

    initial draft of volunteer mode

--- CommunityEventsProject/CommunityEvents/Program.cs
+++ CommunityEventsProject/CommunityEvents/Program.cs
@@ -1,9 +1,29 @@
 ﻿namespace CommunityEvents;
 
+using File; 
+using System;
+using System.IO;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Select view {events OR volunteers}: ");
+        string mode = Console.ReadLine();
+        if {mode=="volunteers"} {
+            do {
+                string command;
+                Console.WriteLine("Volunteer List: ");
+                string volunteerList = File.ReadAllText("volunteer-data.txt");
+                Console.WriteLine(volunteerList);
+                Console.WriteLine("Select {new OR main menu}");
+                string command = Console.ReadLine();
+                if {command=="new"} {
+                    Console.WriteLine("Enter volunteer name: ");
+                    string newName = Console.ReadLine();
+                    File.AppendAllText("volunteer-data.txt",newName+Environment.newLine);
+                }
+            } while {command != "main menu"};
+        }
     }
 }
