48397fb33d095cbdcfe5dcb82ce02c7c8b52a468
windowmipers <98720261+windowmipers@users.noreply.github.com>
2025-04-16 21:18:19 +0000

    Created EventView function

--- CommunityEventsProject/CommunityEvents/Program.cs
+++ CommunityEventsProject/CommunityEvents/Program.cs
@@ -2,6 +2,7 @@
  
 using System;
 using System.IO;
+using System.Collections.Generic;
 
 class Program
 {
@@ -21,14 +22,33 @@ class Program
         string[] eventList = File.ReadAllLines("event-names.txt");
         foreach (string event in eventList) {
             if (command==event) {
+                EventView(event);
+                break;
                 
             }
         }
     }
-    public EventView() {
+    public EventView(string eventName) {
         string[] lines = File.ReadAllLines("event-data.csv");
+        List<string> volunteers = new List<string>();
+        List<string> items = new List<string>();
         foreach (string line in lines) {
             string[] elements = line.Split(',');
+            if (elements[0]==eventName) {
+                if (elements[1]=="Volunteer") {
+                    volunteers.Add(elements[2]);
+                } else if (elements[1]=="Type") {
+                    items.Add(elements[2]);
+                }
+            }
         }
+        Console.WriteLine(eventName + "Information:");
+        Console.WriteLine("Volunteers:");
+        foreach(string volunteer in volunteers) {
+            Console.WriteLine(volunteer);
+        }
+        Console.WriteLine("Items:");
+        foreach(string item in items) {
+            Console.WriteLine(item);
     }
 }
