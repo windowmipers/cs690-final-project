3d42e8c7a7a513a257135aad767b8287139a7940
windowmipers <98720261+windowmipers@users.noreply.github.com>
2025-04-17 01:51:08 +0000

    added EventSelect function with ability to add volunteers and items to an event

--- CommunityEventsProject/CommunityEvents/Program.cs
+++ CommunityEventsProject/CommunityEvents/Program.cs
@@ -14,7 +14,7 @@ class Program
             Console.WriteLine("Events: ");
             string eventNames = File.ReadAllText("event-names.txt");
             Console.WriteLine(eventNames);
-            Console.WriteLine("Enter {new OR exit OR [the event name]}");
+            Console.WriteLine("Enter {new OR exit OR [the event name]}: ");
             command = Console.ReadLine();
             if (command=="new") {
                 Console.WriteLine("Enter event name: ");
@@ -24,7 +24,7 @@ class Program
             string[] eventList = File.ReadAllLines("event-names.txt");
             foreach (string Event in eventList) {
                 if (command==Event) {
-                    EventView(Event);
+                    EventSelect(Event);
                     break;
                 
                 }
@@ -40,13 +40,13 @@ class Program
             if (elements[0]==eventName) {
                 if (elements[1]=="Volunteer") {
                     volunteers.Add(elements[2]);
-                } else if (elements[1]=="Type") {
+                } else if (elements[1]=="Item") {
                     items.Add(elements[2]);
                 }
             }
         }
         Console.WriteLine(eventName + " Information:");
-        Console.WriteLine("Volunteers:");
+        Console.WriteLine("Volunteers: ");
         foreach(string volunteer in volunteers) {
             Console.WriteLine(volunteer);
         }
@@ -54,5 +54,28 @@ class Program
         foreach(string item in items) {
             Console.WriteLine(item);
         }
+    }
+    public static void EventSelect(string eventName) {
+        string eventCommand = "none";
+        string volunteerName = "none";
+        string volunteerData = "none";
+        string itemName = "none";
+        string itemData = "none";
+        do {
+            EventView(eventName);
+            Console.WriteLine("Enter {add volunteer OR add item OR exit}: ");
+            eventCommand = Console.ReadLine();
+            if (eventCommand=="add volunteer") {
+                Console.WriteLine("Enter volunteer name: ");
+                volunteerName = Console.ReadLine();
+                volunteerData = eventName + ',' + "Volunteer" + ',' + volunteerName + ',' + "None";
+                File.AppendAllText("event-data.csv",volunteerData+Environment.NewLine);
+            } else if (eventCommand=="add item") {
+                Console.WriteLine("Enter item name: ");
+                itemName = Console.ReadLine();
+                itemData = eventName + ',' + "Item" + ',' + itemName + ',' + "None";
+                File.AppendAllText("event-data.csv",itemData+Environment.NewLine);
+            }
+        } while (eventCommand != "exit");
     }
 }
