9524b0863756858ed107f29a4dcb10702ad14c70
windowmipers <98720261+windowmipers@users.noreply.github.com>
2025-04-22 06:24:17 +0000

    implemented better UI

--- CommunityEventsProject/CommunityEvents/ConsoleUI.cs
+++ CommunityEventsProject/CommunityEvents/ConsoleUI.cs
@@ -1,35 +1,56 @@
 namespace CommunityEvents;
 
+using Spectre.Console;
+
 public class ConsoleUI {
     public void Show() {
         string command = "none";
+        string selectedEvent = "none";
         do {
             Console.WriteLine("Events: ");
             string eventNames = File.ReadAllText("event-names.txt");
+            string[] eventList = File.ReadAllLines("event-names.txt");
             Console.WriteLine(eventNames);
-            command = AskForInput("Enter {new OR delete OR exit OR [the event name]}: ");
-            if (command=="new") {
+            command = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Select an option: ")
+                    .AddChoices(new[] {
+                        "New Event", "Delete Event", "Select Event", "Exit", 
+                    }));
+            if (command=="New Event") {
                 string newName = AskForInput("Enter event name: ");
                 File.AppendAllText("event-names.txt",newName+Environment.NewLine);
-            } else if (command=="delete") {
-                string eventToDelete = AskForInput("Enter event name: ");
+            } else if (command=="Delete Event") {
+                string eventToDelete = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                        .Title("Select an option: ")
+                        .AddChoices(
+                            eventList
+                        ));
                 string[] lines = File.ReadAllLines("event-names.txt");
                 File.Create("event-names.txt").Close();
                 foreach (string line in lines) {
                     string[] elements = line.Split(',');
                     if (elements[0]!=eventToDelete) {
-                        File.AppendAllText("event-data.csv",line+Environment.NewLine);
+                        File.AppendAllText("event-names.txt",line+Environment.NewLine);
+                    }
+                }
+            } else if (command=="Select Event") {
+                selectedEvent = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                        .Title("Select an option: ")
+                        .AddChoices(
+                            eventList
+                        ));
+            
+                foreach (string Event in eventList) {
+                    if (selectedEvent==Event) {
+                        EventSelect(Event);
+                        break;
                     }
                 }
             }
-            string[] eventList = File.ReadAllLines("event-names.txt");
-            foreach (string Event in eventList) {
-                if (command==Event) {
-                    EventSelect(Event);
-                    break;
-                }
-            }
-        } while (command != "exit");
+        } while (command != "Exit");
     }
     public static void EventView(string eventName) {
         string[] lines = File.ReadAllLines("event-data.csv");
@@ -62,34 +83,92 @@ public class ConsoleUI {
         string newStatus = "none";
         do {
             EventView(eventName);
-            eventCommand = AskForInput("Enter {add volunteer OR add item OR delete volunteer OR delete item OR update volunteer role OR change item status OR exit}: ");
-            if (eventCommand=="add volunteer") {
+            string[] lines = File.ReadAllLines("event-data.csv");
+            List<string> volunteerNames = new List<string>();
+            List<string> itemNames = new List<string>();
+            foreach (string line in lines) {
+                string[] elements = line.Split(',');
+                if (elements[0]==eventName) {
+                    if (elements[1]=="Volunteer") {
+                        volunteerNames.Add(elements[2]);
+                    } else if (elements[1]=="Item") {
+                        itemNames.Add(elements[2]);
+                    }
+                }
+            }
+            eventCommand = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Select an option: ")
+                    .AddChoices(new[] {
+                        "Add Volunteer", "Add Item", "Delete Volunteer", "Delete Item", "Update Volunteer Role", "Change Item Status", "Exit",
+                    }));
+            if (eventCommand=="Add Volunteer") {
                 volunteerName = AskForInput("Enter volunteer name: ");
                 AddVolunteer(eventName,volunteerName);
-            } else if (eventCommand=="add item") {
+            } else if (eventCommand=="Add Item") {
                 itemName = AskForInput("Enter item name: ");
                 AddItem(eventName,itemName);
-            } else if (eventCommand=="delete volunteer") {
-                volunteerName = AskForInput("Enter volunteer name: ");
-                DeleteEntity(eventName,volunteerName);
-            } else if (eventCommand=="delete item") {
-                itemName = AskForInput("Enter item name: ");
+            } else if (eventCommand=="Delete Volunteer") {
+                if (volunteerNames.Count > 0) {
+                    volunteerName = AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                            .Title("Select a volunteer: ")
+                            .AddChoices(
+                                volunteerNames
+                            ));
+                    DeleteEntity(eventName,volunteerName);
+                } else {
+                    Console.WriteLine("No volunteers to select from");
+                }
+            } else if (eventCommand=="Delete Item") {
+                if (itemNames.Count > 0) {
+                    itemName = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                        .Title("Select an item: ")
+                        .AddChoices(
+                            itemNames
+                        ));
+                } else {
+                    Console.WriteLine("No items to select from");
+                }
                 DeleteEntity(eventName,itemName);
-            } else if (eventCommand=="update volunteer role") {
-                volunteerName = AskForInput("Enter volunteer name: ");
-                newStatus = AskForInput("Enter assigned role: ");
-                Update(eventName,"Volunteer",volunteerName,newStatus);
-            } else if (eventCommand=="change item status") {
-                itemName = AskForInput("Enter item name: ");
-                newStatus = AskForInput("Has the item been acquired? {Y OR N} ");
-                if (newStatus=="Y") {
-                    newStatus = "Acquired";
-                } else if (newStatus=="N")
-                    newStatus = "Not Acquired";
-                Update(eventName,"Item",itemName,newStatus);
+            } else if (eventCommand=="Update Volunteer Role") {
+                if (volunteerNames.Count > 0) {
+                    volunteerName = AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                            .Title("Select a volunteer: ")
+                            .AddChoices(
+                                volunteerNames
+                            ));
+                    newStatus = AskForInput("Enter assigned role: ");
+                    Update(eventName,"Volunteer",volunteerName,newStatus);
+                } else {
+                    Console.WriteLine("No volunteers to select from");
+                }
+            } else if (eventCommand=="Change Item Status") {
+                if (itemNames.Count > 0) {
+                    itemName = AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                            .Title("Select an option: ")
+                            .AddChoices(
+                                itemNames
+                            ));
+                    newStatus = AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                            .Title("Has the item been acquired?")
+                            .AddChoices(new[] {
+                                "Yes", "No",
+                            }));
+                    if (newStatus=="Yes") {
+                        newStatus = "Acquired";
+                    } else if (newStatus=="No")
+                        newStatus = "Not Acquired";
+                    Update(eventName,"Item",itemName,newStatus);
+                } else {
+                    Console.WriteLine("No items to select from");
+                }
             }
-                
-        } while (eventCommand != "exit");
+        } while (eventCommand != "Exit");
     }
     public static string AskForInput(string message) {
         Console.Write(message);
